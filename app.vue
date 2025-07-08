ede7495292e51bf9c2551b10804955f80c8f942b
Jorge Martínez <jorgemartinez.jam@gmail.com>
2025-07-08 12:38:41 -0300

    Add vue+nuxt+tailwind config

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-600 to-blue-500 text-white text-4xl font-bold"
+  >
+    🎉 Tailwind está funcionando 🎉
+  </div>
+</template>
